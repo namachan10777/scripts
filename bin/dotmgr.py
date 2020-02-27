6a36f39b5636e5527f3ded2932334b3a17c3fbb3
Nakano Masaki <namachan10777@gmail.com>
2020-02-28 01:25:17 +0900

    configure_envs

--- bin/dotmgr.py
+++ bin/dotmgr.py
@@ -3,11 +3,30 @@
 import argparse
 import json
 from pathlib import Path
+import os
 
 def get_root_of_cfg(cfg_file):
     return Path(cfg_file.name).expanduser().resolve().parent
 
+def parse_cfg_file(cfg_file):
+    return json.loads(cfg_file.read())
+
+def configure_envs(cfg):
+    for env_name in cfg['additional_envs'].keys():
+        env = cfg['additional_envs'][env_name]
+        value = env['value']
+
+        if env['resolve']:
+            value = str(Path(value).expanduser().resolve())
+
+        if env_name not in os.environ or env['type'] == 'overwrite':
+            os.environ[env_name] = value
+        elif env['type'] == 'append':
+            os.environ[env_name] = os.environ[env_name] + ':' + value
+
+
 def check(cfg_file):
+    configure_envs(parse_cfg_file(cfg_file))
     print(get_root_of_cfg(cfg_file))
 
 def deploy(cfg_file):
