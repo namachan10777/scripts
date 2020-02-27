31ae83f51dfbc1ba5d9fd54e92a742db215c98bf
Nakano Masaki <namachan10777@gmail.com>
2020-02-28 00:48:00 +0900

    impl argparser

--- bin/dotmgr.py
+++ bin/dotmgr.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
+import argparse
+import json
+
+def parse_cfg(cfg):
+    return json.loads(cfg.read())
+
+def check(cfg):
+    print('check')
+
+def deploy(cfg):
+    print('deploy')
+
 if __name__ == '__main__':
-    print('Hello World!')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c',  '--check', help='check' , type=open)
+    parser.add_argument('-d', '--deploy', help='deploy', type=open)
+    args = parser.parse_args()
+    if args.check is not None:
+        check(args.check)
+    elif args.deploy is not None:
+        deploy(args.deploy)
+    else:
+        parser.print_help()
