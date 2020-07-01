ba26277f4a7ce545dae46b0084f0c48f10b3edcd
Masaki Nakano <namachan10777@gmail.com>
2020-07-01 23:06:09 +0900

    allow UnFree

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,26 +4,28 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
+  imports =
+    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ];
 
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/fba4ffa7-e75c-4ce7-8891-ad30d621d590";
-    fsType = "xfs";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/fba4ffa7-e75c-4ce7-8891-ad30d621d590";
+      fsType = "xfs";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/AAEC-C880";
-    fsType = "vfat";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/AAEC-C880";
+      fsType = "vfat";
+    };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/4509926c-c87f-4688-ad36-2c56acfe6aa8"; }];
+    [ { device = "/dev/disk/by-uuid/4509926c-c87f-4688-ad36-2c56acfe6aa8"; }
+    ];
 
   nix.maxJobs = lib.mkDefault 8;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
